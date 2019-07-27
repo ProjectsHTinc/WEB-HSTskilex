2a46f2eed4867ebfb6375fd14fb011cb4abb0924
kamalhappysanz <kamal.happysanz@gmail.com>
2019-07-27 18:29:47 +0530

    Update Apicustomermodel.php

--- application/models/Apicustomermodel.php
+++ application/models/Apicustomermodel.php
@@ -1312,9 +1312,12 @@ LEFT JOIN services AS s ON s.id=so.service_id LEFT JOIN main_category AS mc ON s
       // LEFT JOIN service_payments AS spa ON spa.service_order_id=so.id
       // WHERE so.id='$service_order_id' AND so.customer_id='$user_master_id'";
 
-        $service_query="SELECT IFNULL(lu.phone_no,'') as phone_no,IFNULL(spp.full_name,'') AS full_name,IFNULL(spd.owner_full_name,'') AS owner_full_name,st.from_time,st.to_time,s.service_name,s.service_ta_name,IFNULL((SELECT SUM( ad_service_rate_card) FROM service_order_additional AS soa WHERE service_order_id='$service_order_id'),'') as ad_serv_rate,
+        $service_query="SELECT IFNULL(lu.phone_no,'') as phone_no,IFNULL(spp.full_name,'') AS full_name,IFNULL(spd.owner_full_name,'') AS owner_full_name,st.from_time,st.to_time,mc.main_cat_name,mc.main_cat_ta_name,sc.sub_cat_ta_name,sc.sub_cat_name,
+        s.service_name,s.service_ta_name,IFNULL((SELECT SUM( ad_service_rate_card) FROM service_order_additional AS soa WHERE service_order_id='$service_order_id'),'') as ad_serv_rate,
         (SELECT count( service_order_id) FROM service_order_additional AS soa WHERE service_order_id='$service_order_id' ) AS count_add,IFNULL(spa.paid_advance_amount,'') as paid_advance_amount,IFNULL(spa.service_amount,' ') as service_amount,IFNULL(spa.ad_service_amount,'') as ad_service_amount,spa.sgst_amount,spa.cgst_amount,IFNULL(spa.total_service_amount,'') as total_service_amount,IFNULL(spa.net_service_amount,'') as net_service_amount,IFNULL(spa.payable_amount,'') as payable_amount,IFNULL(spa.coupon_id,'') as coupon_id,IFNULL(spa.discount_amt,'') as discount_amt,spa.status,spa.id as payment_id,so.* FROM service_orders  AS so
         LEFT JOIN services AS s ON s.id=so.service_id
+        LEFT JOIN main_category AS mc ON so.main_cat_id=mc.id
+        LEFT JOIN sub_category AS sc ON so.sub_cat_id=sc.id
         LEFT JOIN service_timeslot AS st ON st.id=so.order_timeslot
         LEFT JOIN service_provider_details AS spd ON spd.user_master_id=so.serv_prov_id
         LEFT JOIN service_person_details AS spp ON spp.user_master_id=so.serv_pers_id
@@ -1335,6 +1338,10 @@ LEFT JOIN services AS s ON s.id=so.service_id LEFT JOIN main_category AS mc ON s
 
           $service_list[]=array(
             "service_order_id"=>$rows_service->id,
+            "main_category"=>$rows_service->main_cat_name,
+            "main_category_ta"=>$rows_service->main_cat_ta_name,
+            "sub_category"=>$rows_service->sub_cat_name,
+            "sub_category_ta"=>$rows_service->sub_cat_ta_name,
             "service_name"=>$rows_service->service_name,
             "service_ta_name"=>$rows_service->service_ta_name,
             "contact_person_name"=>$rows_service->contact_person_name,

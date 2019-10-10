0118f9e543568ec7d95acd9f4af76310705e69ba
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-10 12:00:57 +0530

    sms version upgraded

--- application/models/Smsmodel.php
+++ application/models/Smsmodel.php
@@ -10,64 +10,53 @@ Class Smsmodel extends CI_Model
   }
 
 
-   function send_sms($phone,$notes)
-  	{
-          //Your authentication key
-          $authKey = "191431AStibz285a4f14b4";
 
-          //Multiple mobiles numbers separated by comma
-          $mobileNumber = "$phone";
+ function send_sms($phone,$notes)
+ {
 
-          //Sender ID,While using route4 sender id should be 6 characters long.
-          $senderId = "SKILEX";
+   $curl = curl_init();
+   curl_setopt_array($curl, array(
+     CURLOPT_URL => "https://api.msg91.com/api/v2/sendsms?country=91",
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 30,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => "POST",
+     CURLOPT_POSTFIELDS => '{
+                 "sender": "SKILEX",
+                 "route": "4",
+                 "country": "91",
+                 "sms": [
+                 {
+                   "message": "'.$notes.'",
+                   "to": [
+                   "'.$phone.'"
+                   ]
+                 }
+                 ]
+               }',
+     CURLOPT_SSL_VERIFYHOST => 0,
+     CURLOPT_SSL_VERIFYPEER => 0,
+     CURLOPT_HTTPHEADER => array(
+       "authkey: 191431AStibz285a4f14b4",
+       "content-type: application/json"
+     ),
+   ));
 
-          //Your message to send, Add URL encoding here.
-          $message = $notes;
+   $response = curl_exec($curl);
+   $err = curl_error($curl);
 
-          //Define route
-          $route = "transactional";
+   curl_close($curl);
 
-          //Prepare you post parameters
-          $postData = array(
-              'authkey' => $authKey,
-              'mobiles' => $phone,
-              'message' => $notes,
-              'sender' => $senderId,
-              'route' => $route
-          );
-
-          //API URL
-          $url="https://control.msg91.com/api/sendhttp.php";
-
-          // init the resource
-          $ch = curl_init();
-          curl_setopt_array($ch, array(
-              CURLOPT_URL => $url,
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_POST => true,
-              CURLOPT_POSTFIELDS => $postData
-              //,CURLOPT_FOLLOWLOCATION => true
-          ));
+   if ($err) {
+     echo "cURL Error #:" . $err;
+   } else {
+     // echo $response;
+   }
 
 
-          //Ignore SSL certificate verification
-          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-
-          //get response
-          $output = curl_exec($ch);
-
-        
-          //Print error if any
-          if(curl_errno($ch))
-          {
-              echo 'error:' . curl_error($ch);
-          }
-
-          curl_close($ch);
-  	}
-
+ }
 
 
 

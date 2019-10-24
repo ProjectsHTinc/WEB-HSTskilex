a841d1507aea28c298cb56ccb32ec99e7b36a975
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-24 11:08:59 +0530

    Update Transactionmodel.php

--- application/models/Transactionmodel.php
+++ application/models/Transactionmodel.php
@@ -13,8 +13,63 @@ Class Transactionmodel extends CI_Model
 
 
   function daily_cron_job(){
-    $insert="INSERT INTO daily_payment_transaction(serv_prov_id,total_service_per_day) values('1','2')";
-    $result=$this->db->query($insert);
+    $sQuery = "SELECT
+        so.serv_prov_id,
+        so.order_date,
+        COUNT(so.serv_prov_id) AS service_per_day,
+        SUM(sp.net_service_amount) AS service_total_amt,
+        SUM(sp.serv_pro_net_amount) AS serv_prov_comm_amt,
+        SUM(sp.skilex_net_amount) AS skilex_comm_amt,
+        SUM(sp.skilex_tax_amount) AS tax_able_amt,
+        SUM(sp.online_amount) AS online_trans_amt,
+        SUM(sp.offline_amount) AS offline_trans_amt,
+        SUM(sp.online_amount * 0.2) AS online_skile_com_amt,
+        SUM(sp.online_amount * 0.8) AS online_sp_com_amt,
+        SUM(sp.offline_amount * 0.2) AS offline_skile_com_amt,
+        SUM(sp.offline_amount * 0.8) AS offline_sp_com_amt,
+        (SUM(sp.online_amount * 0.8) - SUM(sp.offline_amount * 0.2)) AS pay_to_serv
+      FROM
+      service_orders AS so
+      LEFT JOIN service_payments AS sp  ON so.id = sp.service_order_id
+      LEFT JOIN service_payment_history AS sphh ON sphh.payment_order_id = sp.id
+      WHERE   sp.status = 'Paid'
+      GROUP BY  so.serv_prov_id";
+    $result=$this->db->query($sQuery);
+    if($result->num_rows()==0){
+
+    }else{
+      foreach($result->result() as $rows){
+        $serv_prov_id=$rows->serv_prov_id;
+        $service_per_day=$rows->service_per_day;
+        $order_date=$rows->order_date;
+        $service_total_amt=$rows->service_total_amt;
+        $serv_prov_comm_amt=$rows->serv_prov_comm_amt;
+        $skilex_comm_amt=$rows->skilex_comm_amt;
+        $tax_able_amt=$rows->tax_able_amt;
+        $online_trans_amt=$rows->online_trans_amt;
+        $offline_trans_amt=$rows->offline_trans_amt;
+        $online_skile_com_amt=$rows->online_skile_com_amt;
+        $online_sp_com_amt=$rows->online_sp_com_amt;
+        $offline_skile_com_amt=$rows->offline_skile_com_amt;
+        $offline_sp_com_amt=$rows->offline_sp_com_amt;
+        $pay_to_serv=$rows->pay_to_serv;
+
+        $check="SELECT * FROM daily_payment_transaction WHERE serv_prov_id='$serv_prov_id' and service_date='$order_date'";
+        $result_check=$this->db->query($check);
+        if($result_check->num_rows()==0){
+          $insQuery = "INSERT INTO daily_payment_transaction(serv_prov_id,total_service_per_day,service_date,serv_total_amount,serv_prov_commission_amt,
+                  skilex_commission_amt,online_transaction_amt,offline_transaction_amt,online_skilex_commission,offline_skilex_commission,online_serv_prov_commission,offline_serv_prov_commission,taxable_amount,pay_to_serv_prov,skilex_closing_status,serv_prov_closing_status,created_at)
+                VALUES('$serv_prov_id','$service_per_day','$order_date','$service_total_amt','$serv_prov_comm_amt','$skilex_comm_amt','$online_trans_amt','$offline_trans_amt','$online_skile_com_amt','$online_skile_com_amt','$online_sp_com_amt','$online_sp_com_amt','$tax_able_amt','$pay_to_serv','Nopay','Unpaid',NOW())";
+          $result_ins=$this->db->query($insert);
+        }else{
+
+        }
+
+
+      }
+
+    }
+
   }
 
 

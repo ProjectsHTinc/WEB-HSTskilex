5daa4306ca3c11d4abf6cef954d5b7835701e711
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-24 17:10:47 +0530

    completed service updated in expert

--- application/models/Apispersonmodel.php
+++ application/models/Apispersonmodel.php
@@ -1410,32 +1410,45 @@ public function Services_list($category_id,$sub_category_id)
 
 	public function List_completed_services($user_master_id)
 	{
-		$sQuery = "SELECT
-					A.id,
-					A.service_location,
-					DATE_FORMAT(A.order_date, '%W %M %e %Y') as order_date,
-					A.status,
-					B.main_cat_name,
-					B.main_cat_ta_name,
-					C.sub_cat_name,
-					C.sub_cat_ta_name,
-					D.service_name,
-					D.service_ta_name,
-					E.from_time,
-					E.to_time,
-					F.owner_full_name AS service_provider,
-					G.status AS Payment_status
-				FROM
-					service_orders A,
-					main_category B,
-					sub_category C,
-					services D,
-					service_timeslot E,
-					service_provider_details F,
-					service_payments G
-				WHERE
-					 A.serv_pers_id = '".$user_master_id."' AND A.status = 'Completed' OR A.Status = 'Paid' AND A.`main_cat_id` = B.id AND A.`sub_cat_id` = C.id AND A.`service_id` = D.id AND A.`order_timeslot` = E.id AND A.serv_prov_id = F.user_master_id AND A.id=G.service_order_id";
-		$serv_result = $this->db->query($sQuery);
+		// $sQuery = "SELECT
+		// 			A.id,
+		// 			A.service_location,
+		// 			DATE_FORMAT(A.order_date, '%W %M %e %Y') as order_date,
+		// 			A.status,
+		// 			B.main_cat_name,
+		// 			B.main_cat_ta_name,
+		// 			C.sub_cat_name,
+		// 			C.sub_cat_ta_name,
+		// 			D.service_name,
+		// 			D.service_ta_name,
+		// 			E.from_time,
+		// 			E.to_time,
+		// 			F.owner_full_name AS service_provider,
+		// 			G.status AS Payment_status
+		// 		FROM
+		// 			service_orders A,
+		// 			main_category B,
+		// 			sub_category C,
+		// 			services D,
+		// 			service_timeslot E,
+		// 			service_provider_details F,
+		// 			service_payments G
+		// 		WHERE
+		// 			 A.serv_pers_id = '".$user_master_id."'
+    //        AND A.status = 'Completed' OR A.Status = 'Paid' AND A.`main_cat_id` = B.id AND A.`sub_cat_id` = C.id AND A.`service_id` = D.id AND A.`order_timeslot` = E.id AND A.serv_prov_id = F.user_master_id AND A.id=G.service_order_id";
+    $sQuery="SELECT so.id,so.service_location,DATE_FORMAT(so.order_date, '%W %M %e %Y') as order_date,DATE_FORMAT(so.resume_date, '%e-%m-%Y') as resume_date,sppd.owner_full_name as service_provider,
+sp.status as Payment_status,so.contact_person_name,so.contact_person_number,so.service_rate_card,mc.main_cat_name,mc.main_cat_ta_name,sc.sub_cat_ta_name,sc.sub_cat_name,s.service_name,s.service_ta_name,st.from_time,st.to_time,so.status,so.start_datetime,so.material_notes,so.serv_prov_id,spd.full_name as service_person,IFNULL(rs.from_time, '') as r_fr_time,IFNULL(rs.to_time, '') as r_to_time
+    from service_orders as so
+    LEFT JOIN services AS s ON s.id=so.service_id
+    LEFT JOIN main_category AS mc ON so.main_cat_id=mc.id
+    LEFT JOIN sub_category AS sc ON so.sub_cat_id=sc.id
+    LEFT JOIN service_timeslot AS st ON st.id=so.order_timeslot
+    LEFT JOIN service_timeslot AS rs ON rs.id=so.resume_timeslot
+    LEFT JOIN service_person_details AS spd ON spd.user_master_id=so.serv_pers_id
+    LEFT JOIN service_provider_details as sppd on so.serv_prov_id=sppd.user_master_id
+    LEFT JOIN service_payments as sp on sp.service_order_id=so.id
+    where so.serv_pers_id='$user_master_id'  and (so.status='Completed' or so.status='Paid')";
+    $serv_result = $this->db->query($sQuery);
 		$service_result = $serv_result->result();
 
 		if($serv_result->num_rows()>0) {

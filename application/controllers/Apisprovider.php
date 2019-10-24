1166ff622aaa1a728ab1ffe90e454238548dbffa
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-24 18:22:41 +0530

    Update Apisprovider.php

--- application/controllers/Apisprovider.php
+++ application/controllers/Apisprovider.php
@@ -1556,7 +1556,7 @@ class Apisprovider extends CI_Controller
 
     public function cancel_service_reasons()
     {
-        $_POST = json_decode(file_get_contents("php://input"), TRUE);
+       $_POST = json_decode(file_get_contents("php://input"), TRUE);
 
         if (!$this->checkMethod()) {
             return FALSE;

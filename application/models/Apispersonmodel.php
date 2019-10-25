31ae92c2e91a2d5bc50ba4733ffc13754f765ea7
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-25 17:48:35 +0530

    email updation
    committed

--- application/models/Apispersonmodel.php
+++ application/models/Apispersonmodel.php
@@ -285,7 +285,7 @@ class Apispersonmodel extends CI_Model {
 
 	public function Profile_update($user_master_id,$full_name,$gender,$email)
 	{
-		$update_sql= "UPDATE service_person_details SET full_name='$full_name',gender='$gender', updated_at=NOW(),updated_by='$user_master_id' WHERE user_master_id='$user_master_id'";
+		$update_sql= "UPDATE service_person_details SET full_name='$full_name',email='$email',gender='$gender', updated_at=NOW(),updated_by='$user_master_id' WHERE user_master_id='$user_master_id'";
 		$update_result = $this->db->query($update_sql);
 
 		$response = array("status" => "success", "msg" => "Profile Updated");
--- application/models/Apisprovidermodel.php
+++ application/models/Apisprovidermodel.php
@@ -468,7 +468,7 @@ class Apisprovidermodel extends CI_Model
     //#################### Profile Update ####################//
     public function Profile_update($user_master_id, $full_name, $gender, $email)
     {
-        $update_sql    = "UPDATE service_provider_details SET owner_full_name='$full_name',gender='$gender',updated_at=NOW(),updated_by='$user_master_id' WHERE user_master_id='$user_master_id'";
+        $update_sql    = "UPDATE service_provider_details SET owner_full_name='$full_name',gender='$gender',email='$email',updated_at=NOW(),updated_by='$user_master_id' WHERE user_master_id='$user_master_id'";
         $update_result = $this->db->query($update_sql);
 
         $response = array(

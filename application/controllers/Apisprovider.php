d6dff4e881c6ca23a0871459e6eb5bba43f3226b
kamalhappysanz <kamal.happysanz@gmail.com>
2019-11-13 12:41:34 +0530

    post removed

--- application/controllers/Apisprovider.php
+++ application/controllers/Apisprovider.php
@@ -1493,7 +1493,7 @@ class Apisprovider extends CI_Controller
 
     public function list_completed_services()
     {
-        //$_POST = json_decode(file_get_contents("php://input"), TRUE);
+        $_POST = json_decode(file_get_contents("php://input"), TRUE);
 
         if (!$this->checkMethod()) {
             return FALSE;

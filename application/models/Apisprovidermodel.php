3332fea3e90e7aa2d0264a74bc292f868c0cc0ac
kamalhappysanz <kamal.happysanz@gmail.com>
2019-11-28 17:43:56 +0530

    accept service committed

--- application/models/Apisprovidermodel.php
+++ application/models/Apisprovidermodel.php
@@ -1506,9 +1506,10 @@ return $response;
     public function Accept_requested_services($user_master_id, $service_order_id)
     {
 
-      $check_order="SELECT * FROM service_orders WHERE status='Pending' AND id='$service_order_id'";
+       $check_order="SELECT * FROM service_orders WHERE status='Pending' AND id='$service_order_id'";
       $result_ch_order=$this->db->query($check_order);
-      if($result_ch_order->num_rows()==0){
+
+      if($result_ch_order->num_rows()==1){
         $update_sql    = "UPDATE service_orders SET serv_prov_id = '" . $user_master_id . "', status  = 'Accepted', updated_by  = '" . $user_master_id . "', updated_at =NOW() WHERE id ='" . $service_order_id . "'";
         $update_result = $this->db->query($update_sql);
 

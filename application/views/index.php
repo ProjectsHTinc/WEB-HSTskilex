cc3e143353837d697a1c617335efd2dee258215b
kamalhappysanz <kamal.happysanz@gmail.com>
2020-01-24 12:34:26 +0530

    instagram link updated committed

--- application/views/index.php
+++ application/views/index.php
@@ -640,7 +640,7 @@ From people who are capable</h2>
                                 <a href="https://www.facebook.com/Skilex-109383073726609/?modal=admin_todo_tour" target="_blank"><i class="icofont-facebook"></i></a>
                                 <a href="https://twitter.com/HomeSkilex" target="_blank"><i class="icofont-twitter"></i></a>
                                 <a href="https://in.pinterest.com/dhanasekarskilex/pins/" target="_blank"><i class="icofont-pinterest"></i></a>
-                                <a href="https://www.instagram.com/skilex_bulletin123/?hl=en" target="_blank"><i class="icofont-instagram"></i></a>
+                                <a href="https://www.instagram.com/skil_ex/" target="_blank"><i class="icofont-instagram"></i></a>
                             </div>
                         </div>
                     </div>

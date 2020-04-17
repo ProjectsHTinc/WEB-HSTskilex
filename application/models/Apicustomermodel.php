49aa966746470740077b9709a76ad8681a16c1b4
kamalhappysanz <kamal.happysanz@gmail.com>
2020-04-17 19:01:46 +0530

    Feedback answer committed

--- application/models/Apicustomermodel.php
+++ application/models/Apicustomermodel.php
@@ -3030,7 +3030,7 @@ function proceed_for_payment($user_master_id,$service_order_id){
 
     function customer_feedback_answer($user_master_id,$service_order_id,$feedback_id,$feedback_text){
 
-      $query="SELECT * FROM feedback_response WHERE service_order_id='$service_order_id' and query_id='$feedback_id'";
+      $query="SELECT * FROM feedback_response WHERE service_order_id='$service_order_id' and query_id='$feedback_id'  and user_master_id='$user_master_id'";
       $res=$this->db->query($query);
       if($res->num_rows()==0){
         $insert="INSERT INTO feedback_response  (user_master_id,service_order_id,query_id,answer_text,status,created_at,created_by) VALUES ('$user_master_id','$service_order_id','$feedback_id','$feedback_text','Active',NOW(),'$user_master_id')";
@@ -3041,7 +3041,7 @@ function proceed_for_payment($user_master_id,$service_order_id){
             $response = array("status" => "error", "msg" => "Something went wrong","msg_en"=>"Oops! Something went wrong!","msg_ta"=>"எதோ தவறு நடந்துள்ளது!");
         }
       }else{
-        $update="UPDATE feedback_response SET answer_text='$feedback_text',created_at=NOW() WHERE service_order_id='$service_order_id' and query_id='$feedback_id'";
+        $update="UPDATE feedback_response SET answer_text='$feedback_text',created_at=NOW() WHERE service_order_id='$service_order_id' and query_id='$feedback_id' and user_master_id='$user_master_id'";
         $result=$this->db->query($update);
         if($result){
           $response=array("status"=>"success","msg"=>"Feedback added successfully","msg_en"=>"","msg_ta"=>"");

d682ed67e520cc7b396ab0339bc4c7652dd866bd
Maran <maran.happysanz@gmail.com>
2020-12-02 17:33:32 +0530

    Update tax_invoice_list.php

--- application/views/admin/transactions/tax_invoice_list.php
+++ application/views/admin/transactions/tax_invoice_list.php
@@ -6,7 +6,7 @@
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb breadcrumb-custom">
               <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>dashboard">Dashboard</a></li>
-              <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>verifyprocess/get_vendor_verify_list">Service Orders </a></li>
+              <li class="breadcrumb-item"><a href="#">Transactions </a></li>
               <li class="breadcrumb-item active" aria-current="page"><span> Invoice </span></li>
             </ol>
           </nav>
